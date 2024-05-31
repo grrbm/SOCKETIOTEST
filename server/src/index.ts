47eb7f3ef154cc2cc1474fc9b2b16995f0755d0b
Guilherme Reis <grrbm2@gmail.com>
2024-05-31 12:04:54 -0300

    added express server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,24 @@
-const greet = (name: string): string => {
-  return `Hello, ${name}!`;
-};
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
 
-console.log(greet("World"));
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const PORT = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World</h1>");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
