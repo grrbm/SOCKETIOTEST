5fcefa62d14dfe98e356114832d17cb3864f6947
Guilherme Reis <grrbm2@gmail.com>
2024-05-31 12:16:50 -0300

    added socket.io client

--- test-project-frontend/src/pages/index.tsx
+++ test-project-frontend/src/pages/index.tsx
@@ -1,5 +1,20 @@
-import Image from "next/image";
+import { useEffect } from "react";
+import { io } from "socket.io-client";
 
 export default function Home() {
+  useEffect(() => {
+    const socket = io("http://localhost:4000");
+
+    socket.on("connect", () => {
+      console.log("Connected to server");
+    });
+
+    // Add any other event listeners here
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
   return <div>Hello World</div>;
 }
